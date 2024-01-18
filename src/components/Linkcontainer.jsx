ed73bba4916bcadf505b48d11b73a100433c8ec5
Karan Joshi <joshikaran.aad.0007@gmail.com>
2024-01-18 10:54:31 +0530

    Added redirectTo mechanisms

--- src/components/Linkcontainer.jsx
+++ src/components/Linkcontainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import instagram from "../assets/images/instagram.png"
 import github from "../assets/images/github.png"
 import twitter from "../assets/images/twitter.png"
@@ -12,33 +12,66 @@ const Linkcontainer = () => {
     const TwitterURL = "https://twitter.com/KaranJoshi128";
     const GithubURL = "https://github.com/Karanjoshi128";
 
+    const [redirectToLinkedin, setRedirectToLinkedin] = useState(false);
+    const [redirectToTwitter, setRedirectToTwitter] = useState(false);
+    const [redirectToInstagram, setRedirectToInstagram] = useState(false);
+    const [redirectToGithub, setRedirectToGithub] = useState(false);
+
+
+
+    // functions to copy socials
+
+
     const copyLinkedInURL = () => {
         navigator.clipboard.writeText(LinkedInURL);
+        setRedirectToLinkedin(true);
     }
     const copyInstaURL = () => {
         navigator.clipboard.writeText(InstaURL);
+        setRedirectToInstagram(true);
     }
     const copyTwitterURL = () => {
         navigator.clipboard.writeText(TwitterURL);
+        setRedirectToTwitter(true);
     }
     const copyGithubURL = () => {
         navigator.clipboard.writeText(GithubURL);
+        setRedirectToGithub(true); 
     }
 
+    
+    //functions to redirect to socials
 
+
+    const redirectLinkedInURL = () => {
+        location.href = LinkedInURL;
+        setRedirectToLinkedin(false);
+    }
+    const redirectInstaURL = () => {
+        location.href = InstaURL;
+        setRedirectToInstagram(false);
+    }
+    const redirectTwitterURL = () => {
+        location.href = TwitterURL;
+        setRedirectToTwitter(fasle);
+    }
+    const redirectGithubURL = () => {
+        location.href = GithubURL;
+        setRedirectToGithub(false);
+    }
 
     return (
         <div className='h-[30rem] w-[20rem] bg-slate-300 rounded-xl flex flex-wrap justify-start gap-0 content-start relative shadow-2xl'>
-            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1.2rem] mt-[1.5rem]  shadow-md hover:shadow-xl cursor-pointer'  onClick={copyLinkedInURL} >
-                <img src={linkedin} alt="" className='h-[2.5rem] w-[2.5rem] shadow-xl ' defaultValue={LinkedInURL} />
+            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1.2rem] mt-[1.5rem]  shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToLinkedin ? redirectLinkedInURL : copyLinkedInURL} >
+                <img src={linkedin} alt="" className='h-[2.5rem] w-[2.5rem] shadow-xl '/>
             </div>
-            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={copyGithubURL} >
+            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToGithub ? redirectGithubURL : copyGithubURL} >
                 <img src={github} alt="" className='h-[2.5rem] w-[2.5rem] ' />
             </div>
-            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={copyInstaURL} >
+            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToInstagram ? redirectInstaURL : copyInstaURL} >
                 <img src={instagram} alt="" className='h-[2.5rem] w-[2.5rem] ' />
             </div>
-            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={copyTwitterURL} >
+            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToTwitter ? redirectTwitterURL :  copyTwitterURL} >
                 <img src={twitter} alt="" className='h-[2.5rem] w-[2.5rem] ' />
             </div>
             <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer'>
@@ -52,9 +85,3 @@ const Linkcontainer = () => {
 }
 
 export default Linkcontainer
-// position: absolute;
-//     border: 2px solid black;
-//     width: 15rem;
-//     bottom: 3rem;
-//     left: 2.5rem;
-// }
