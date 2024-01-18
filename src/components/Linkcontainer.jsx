dcd0c93a6c381b40944887f662ba91904d009180
Karan Joshi <joshikaran.aad.0007@gmail.com>
2024-01-18 11:28:40 +0530

    Added some onBoard instructions for my copt&redirect mechanisms to improve UX

--- src/components/Linkcontainer.jsx
+++ src/components/Linkcontainer.jsx
@@ -36,10 +36,10 @@ const Linkcontainer = () => {
     }
     const copyGithubURL = () => {
         navigator.clipboard.writeText(GithubURL);
-        setRedirectToGithub(true); 
+        setRedirectToGithub(true);
     }
 
-    
+
     //functions to redirect to socials
 
 
@@ -63,7 +63,7 @@ const Linkcontainer = () => {
     return (
         <div className='h-[30rem] w-[20rem] bg-slate-300 rounded-xl flex flex-wrap justify-start gap-0 content-start relative shadow-2xl'>
             <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1.2rem] mt-[1.5rem]  shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToLinkedin ? redirectLinkedInURL : copyLinkedInURL} >
-                <img src={linkedin} alt="" className='h-[2.5rem] w-[2.5rem] shadow-xl '/>
+                <img src={linkedin} alt="" className='h-[2.5rem] w-[2.5rem] shadow-xl ' />
             </div>
             <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToGithub ? redirectGithubURL : copyGithubURL} >
                 <img src={github} alt="" className='h-[2.5rem] w-[2.5rem] ' />
@@ -71,13 +71,21 @@ const Linkcontainer = () => {
             <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToInstagram ? redirectInstaURL : copyInstaURL} >
                 <img src={instagram} alt="" className='h-[2.5rem] w-[2.5rem] ' />
             </div>
-            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToTwitter ? redirectTwitterURL :  copyTwitterURL} >
+            <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer' onClick={redirectToTwitter ? redirectTwitterURL : copyTwitterURL} >
                 <img src={twitter} alt="" className='h-[2.5rem] w-[2.5rem] ' />
             </div>
             <div className='bg-white h-[4.5rem] w-[4.5rem] flex justify-center items-center rounded-md m-[1rem] mt-[1.5rem] shadow-md hover:shadow-xl cursor-pointer'>
                 <img src={AddMore} alt="" className='h-[2.5rem] w-[2.5rem] ' />
             </div>
             <div className='border-[1px] w-[17rem] border-black h-[0.1rem] absolute bottom-[3.3rem] left-[1.5rem] ' ></div>
+            <div className='h-[5rem] w-[20rem] mt-[7rem] ml-[1rem] mr-[1rem] p-1'>
+                <pre className='font-medium'>
+                    1. Tap once to copy the URL to <br/>   your clipboard.
+                </pre>
+                <pre className='font-medium' >
+                    2. Tap twice to visit the URL.
+                </pre>
+            </div>
 
             <span className='font-extrabold absolute text-2xl bottom-[0.8rem] right-[7.5rem]'  >LYNKIE</span>
         </div>
